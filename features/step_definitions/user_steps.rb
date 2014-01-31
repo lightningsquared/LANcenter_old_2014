ea985bea4cb66717fa83a024614fadfb9262f9c8
Ariel Kirkwood <ariel.kirkwood@me.com>
2014-01-31 00:09:23 -0500

    first paste dump of user account steps generated by Cuke

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,75 @@
+Given(/^no user accounts exist$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I submit a new user account request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I confirm my email address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a new user account should be created$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user account should be of type "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^one or more user accounts exist of type "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I access any location within LANcenter that is not the new user account page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be redirected to the user account creation page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be notified that an event organizer user account must first be created$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^one user account exists of type "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I try to delete the user account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user account should not be deleted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be notified that at least one event organizer user account must exist$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an event exists$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an attendee has registered for an event$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the attendee submits a new user account request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the email address in the new user account request matches an email address in the list of attendees from any event$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the attendee confirms their email address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the attendee's information should be connected to the attendee's user account$/) do
+  pending # express the regexp above with the code you wish you had
+end
