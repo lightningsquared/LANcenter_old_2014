f99774d8a89191f88d8699913c7efe26d77f682a
Ariel Kirkwood <ariel.kirkwood@me.com>
2014-03-12 21:45:49 -0400

    organization migration + updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140309154029) do
+ActiveRecord::Schema.define(:version => 20140312173959) do
+
+  create_table "organizations", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",         :null => false
